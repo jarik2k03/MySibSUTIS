85f6e1d4862abd957095533da606705af29cd8ad
Yaroslav2003 <iv121s05@DESKTOP-4LNPDOU.localdomain>
2023-05-06 23:26:16 +0700

    cmath::completed curs

---
+++ cmath/curs/exp/draw.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+gnuplot ./plotS.info
+gnuplot ./plotE.info
+gnuplot ./plotI.info
+gnuplot ./plotR.info
+gnuplot ./plotD.info
---
+++ cmath/curs/sources/euler/euler.h
@@ -0,0 +1,2 @@
+void euler_modified (int a, int b, int h, double *S, double *E, double *I,
+                     double *R, double *D);
